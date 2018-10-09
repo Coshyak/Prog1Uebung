71793441c0925b3276615f771f34701801a42924
Coshyak <florian.hergenroeder@gmail.com>
2018-10-09 11:53:46 +0200

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Lästig");
+        blatt1Aufgabe1();
+    }
+
+
+    public static void blatt1Aufgabe1() {
+        int n;
+        int Ausgabe;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Bitte Zahl eingeben: ");
+        n = scan.nextInt();
+        Ausgabe = n % 17;
+        System.out.println("Der Rest bei Division durch 17 ist " + Ausgabe);
+        if (Ausgabe == 13) {
+            System.out.println("Pechkeks");
+        }
     }
 }
