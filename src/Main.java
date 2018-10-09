280a0319ff6c1dd311c96d909fdf84e45edc165b
Coshyak <florian.hergenroeder@gmail.com>
2018-10-09 13:25:17 +0200

    Aufgabe 1 und 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Lästig");
-        blatt1Aufgabe1();
+        blatt2Aufgabe1(true,false);
     }
 
 
@@ -18,4 +18,68 @@ public class Main {
             System.out.println("Pechkeks");
         }
     }
+    public static void blatt2Aufgabe1() {
+        boolean x;
+        boolean y;
+        boolean z = false;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Bitte Wert x eingeben: ");
+        x = scan.nextBoolean();
+        System.out.println("Bitte Wert y eingeben: ");
+        y = scan.nextBoolean();
+        if((x == false) && (y == false)){
+            z = true;
+        }
+        if((x == false) && (y == true)){
+            z = true;
+        }
+        if((x == true) && (y == false)){
+            z = true;
+        }
+        if((x == true) && (y == true)){
+            z = false;
+        }
+        System.out.println(z);
+    }
+    public static void blatt2Aufgabe2() {
+      int x;
+      int y;
+      int max;
+      Scanner scan = new Scanner(System.in);
+        System.out.println("Bitte einen Wert für x eingeben: ");
+        x = scan.nextInt();
+        System.out.println("Bitte einen Wert für y eingeben: ");
+        y = scan.nextInt();
+        if((x > 0) || (y > 0)){
+            if(x > y){
+              max = x;
+            } else{
+                max = y;
+            }
+        } else{
+            max = 0;
+        }
+        System.out.println(max);
+    }
+    public static void blatt2Aufgabe3(){
+        int n;
+        Scanner scan = new Scanner(System.in);
+        n = scan.nextInt();
+        for(int i = 1; i <= n; i = i + 2){
+            System.out.println(i);
+        }
+        for(int i = 1; i <= n; i++) {
+            System.out.println(i * i);
+        }
+        for(int i = 1; i <= n; i++) {
+            System.out.println(i * 13);
+        }
+        for(int i = n; i >= 0; i--){
+            System.out.println(i);
+        }
+    }
+    public static void blatt2Aufgabe4(){
+
+    }
 }
+
